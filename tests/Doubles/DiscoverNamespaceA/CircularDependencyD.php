d4dc192adef182c450a4df885a61929b1ae52dc8
Felipe Martins <6557756+fefas@users.noreply.github.com>
2021-03-28 14:40:22 +0200

    Rename attribute
    Co-authored-by: Eduardo Dobay <edudobay@users.noreply.github.com>

--- tests/Doubles/DiscoverNamespaceA/CircularDependencyD.php
+++ tests/Doubles/DiscoverNamespaceA/CircularDependencyD.php
@@ -5,7 +5,7 @@ namespace Bauhaus\Doubles\DiscoverNamespaceA;
 class CircularDependencyD
 {
     public function __construct(
-        private CircularDependencyC $a,
+        private CircularDependencyC $c,
     ) {
     }
 }
